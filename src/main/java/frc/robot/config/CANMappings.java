4e534c851742b08729a06c92cbcca215fe8dec6a
1062987 <adham.m.yeh@gmail.com>
2025-10-28 16:45:35 -0400

    intake code

--- src/main/java/frc/robot/config/CANMappings.java
+++ src/main/java/frc/robot/config/CANMappings.java
@@ -1,3 +1,7 @@
 package frc.robot.config;
 
-public class CANMappings {}
+public class CANMappings {
+
+  public static final int K_INITIAL_INTAKE_ID = 0;
+  public static final int K_KICKER_INTAKE_ID = 0;
+}
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,3 +1,26 @@
 package frc.robot.subsystems;
 
-public class Intake {}
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.config.CANMappings;
+import frc.robot.config.IntakeConfig;
+
+public class Intake {
+  protected TalonFX mInitialIntake;
+  protected TalonFX mKickerIntake;
+
+  public Intake() {
+    mInitialIntake = new TalonFX(CANMappings.K_INITIAL_INTAKE_ID);
+    mKickerIntake = new TalonFX(CANMappings.K_KICKER_INTAKE_ID);
+
+    TalonFXConfiguration initialIntakeConfig = new TalonFXConfiguration();
+    TalonFXConfiguration kickerIntakeConfig = new TalonFXConfiguration();
+
+    initialIntakeConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    initialIntakeConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    kickerIntakeConfig.CurrentLimits.SupplyCurrentLimit =
+        IntakeConfig.K_INTAKE_MOTORS_SUPPLY_CURRENT_LIMIT;
+    initialIntakeConfig.CurrentLimits.SupplyCurrentLimit =
+        IntakeConfig.K_INTAKE_MOTORS_SUPPLY_CURRENT_LIMIT;
+  }
+}
