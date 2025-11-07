72332c358cfa3574af42e336061d866270bc3e26
graysodastudios-png <graysodastudios@gmail.com>
2025-11-07 15:28:14 -0500

    shooter code

--- src/main/java/frc/robot/config/CANMappings.java
+++ src/main/java/frc/robot/config/CANMappings.java
@@ -1,3 +1,6 @@
 package frc.robot.config;
 
-public class CANMappings {}
+public class CANMappings {
+    public static final int K_TOP_SHOOTER_ID = 0;
+    public static final int K_BOTTOM_SHOOTER_ID = 0;
+}
