0c41753f18af5f82723a9d044e2ba7eab5808309
sophiep29 <186753525+sophiep29@users.noreply.github.com>
2025-11-09 12:29:18 -0500

    fixed logging for pivot file

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -6,12 +6,13 @@ import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.config.CANMappings;
 import frc.robot.config.PivotConfig;
 
+@Logged
 public class Pivot extends SubsystemBase {
   protected TalonFX mPivotLeft;
   protected TalonFX mPivotRight;
@@ -120,13 +121,5 @@ public class Pivot extends SubsystemBase {
     currentAngle = currentAngle * 360;
 
     return currentAngle;
-  }
-
-  @Override
-  public void periodic() {
-    SmartDashboard.putBoolean("Pivot At Setpoint", pivotAtSetpoint());
-    SmartDashboard.putNumber("Pivot Actual Angle Degrees", getPivotAngleDegrees());
-    SmartDashboard.putNumber(
-        "Pivot Goal Angle Degrees", mPivotLeft.getClosedLoopReference().getValueAsDouble() * 360);
   }
 }
