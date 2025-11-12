6cb3ff855dde1830176fd812ce322513ca959bc1
literal-gargoyle <mochithecorgi0117@gmail.com>
2025-11-12 16:40:02 -0500

    Added Config for VisionConfig.java, as well as PhotonCamera cams for Vision.java

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,5 +1,14 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.VisionConfig;
+import org.photonvision.PhotonCamera;
 
-public class Vision extends SubsystemBase {}
+public class Vision extends SubsystemBase {
+  public static final PhotonCamera mainCamera = new PhotonCamera(VisionConfig.MAIN_CAMERA_NAME); // Might be used for aprilTags? but i dunno
+
+  public static final PhotonCamera aprilLeftCamera =
+      new PhotonCamera(VisionConfig.APRIL_LEFT_CAMERA_NAME);
+  public static final PhotonCamera aprilRightCamera =
+      new PhotonCamera(VisionConfig.APRIL_RIGHT_CAMERA_NAME);
+}
