2636bb320de88ee8fe5186ca11e89926d9504894
Brayden Zee <github@blueish.dev>
2025-12-05 19:03:23 -0500

    Register the drivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
@@ -90,6 +91,8 @@ public class RobotContainer {
 
   // links xbox controller to controls
   public RobotContainer() {
+
+      CommandScheduler.getInstance().registerSubsystem(drivetrain);
 
     SmartDashboard.putData("Field", m_field);
 
